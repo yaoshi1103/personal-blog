f77551b1eb703033ae08c220ad11bb3161db3cc5
Blue <yaoshi1019@163.com>
2026-06-24 18:20:51 +0800

    feat: 更换个人头像

--- src/config.ts
+++ src/config.ts
@@ -48,7 +48,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Blue",
 	bio: "你好，我是 Blue，这是我的个人博客。",
 	links: [
